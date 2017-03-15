9791d45f6b3f3b66e1b1eea0188f4556b738d740
PRITESH SRIVASTAVA <anshu.68ec@gmail.com>
2017-03-16 00:48:22 +0530

    Update Proj_4.py

--- Proj_4.py
+++ Proj_4.py
@@ -4,24 +4,16 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
-z = []
-l =[]
-
-for i in range(100, 1000):
-    # print("i =", i, ":", end=" ")
-    for j in range(100, 1000):
-        p = i*j
-        z.append(p)
-    #     print("{:2d}".format(i * j), end=" ")
-    # print()
-
-def is_palindrome(i):
-    s = str(i)
-    if s == s[::-1]:
-        return True
-for digit in z:
-    if (is_palindrome(digit)) == True:
-        l.append(digit)
-print(max(l))
+a=[]
+b=[]
+for i in range(100,1000):
+    for j in range(100,1000):
+        x = i*j
+        a.append(x)
+for y in a:
+    z = str(y)
+    if z == z[::-1]:
+        b.append(y)
+print (max(b))
 
 
